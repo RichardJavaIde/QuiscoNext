bb06bfc69d357f6f527f0d577f14276a5c938275
unknown <duran41001@gmail.com>
2024-08-13 20:45:23 -0400

    ya se pueden poner la orden como completada y recarga la pagina automatico para ver las ordenes que estan pendiente.

--- components/order/OrderCard.tsx
+++ components/order/OrderCard.tsx
@@ -1,3 +1,4 @@
+import { completeOrder } from "@/actions/complete-order-action";
 import { formatCurrency } from "@/src/lib/utils";
 import { OrderWithProducts } from "@/src/types";
 
@@ -9,13 +10,28 @@ export default function OrderCard({ order }: OrderCardProps) {
   return (
     <section
       aria-labelledby="summary-heading"
-      className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6  lg:mt-0 lg:p-8 space-y-4"
+      className=" border border-gray-400  mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6  lg:mt-0 lg:p-8 space-y-4"
     >
       <p className="text-2xl font-medium text-gray-900">
         Cliente: {order.name}
       </p>
       <p className="text-lg font-medium text-gray-900">Productos Ordenados:</p>
       <dl className="mt-6 space-y-4">
+        {order.orderProducts.map((product) => (
+          <div
+            key={product.producId}
+            className=" flex items-center gap-2 border-t border-gray-200 pt-4"
+          >
+            <dt className="flex items-center text-sm text-gray-600">
+              <span className="font-black">
+                ({product.quantity}){""}
+              </span>
+            </dt>
+            <dd className="text-sm font-medium text-gray-900">
+              {product.product.name}
+            </dd>
+          </div>
+        ))}
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
           <dt className="text-base font-medium text-gray-900">
             Total a Pagar: {formatCurrency(order.total)}
@@ -24,7 +40,8 @@ export default function OrderCard({ order }: OrderCardProps) {
         </div>
       </dl>
 
-      <form>
+      <form action={completeOrder}>
+        <input type="hidden" value={order.id} name="order_id" />
         <input
           type="submit"
           className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
